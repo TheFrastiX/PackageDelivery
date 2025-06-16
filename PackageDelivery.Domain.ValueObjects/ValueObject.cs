a827eded3fd68d0589c15f682b33b04d8a725994
DimOn <dima05079@bk.ru>
2025-06-16 14:49:01 +0700

    Create ValueObject.cs

---
+++ PackageDelivery.Domain.ValueObjects/ValueObject.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+
+namespace DeliveryParcel.Domain.ValueObjects
+{
+    /// <summary>
+    /// Базовый класс для всех Value Objects.
+    /// Реализует сравнение по значению.
+    /// </summary>
+    public abstract class ValueObject
+    {
+        protected abstract IEnumerable<object> GetEqualityComponents();
+
+        public override bool Equals(object? obj)
+        {
+            if (obj == null)
+                return false;
+
+            if (GetType() != obj.GetType())
+                return false;
+
+            var valueObject = (ValueObject)obj;
+
+            return GetEqualityComponents().SequenceEqual(valueObject.GetEqualityComponents());
+        }
+
+        public override int GetHashCode()
+        {
+            return GetEqualityComponents()
+                .Aggregate(1, (current, obj) => current * 23 + (obj?.GetHashCode() ?? 0));
+        }
+
+        public static bool operator ==(ValueObject left, ValueObject right)
+        {
+            return Equals(left, right);
+        }
+
+        public static bool operator !=(ValueObject left, ValueObject right)
+        {
+            return !(left == right);
+        }
+    }
+}
