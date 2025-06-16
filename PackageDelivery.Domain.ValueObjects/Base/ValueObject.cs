cd00d48ec3cde3081b984da9bf5f94bfda20f72c
DimOn <dima05079@bk.ru>
2025-06-16 13:04:29 +0700

    Update ValueObject.cs

--- PackageDelivery.Domain.ValueObjects/Base/ValueObject.cs
+++ PackageDelivery.Domain.ValueObjects/Base/ValueObject.cs
@@ -1,43 +1,31 @@
-using AuctionTrading.Domain.ValueObjects.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
-namespace AuctionTrading.Domain.ValueObjects.Base
+namespace ValueObjects.Base
 {
-    public abstract class ValueObject<T> : IEquatable<ValueObject<T>>
+
+    public abstract class ValueObject : IEquatable<ValueObject>
     {
-        public T Value { get; }
+        protected abstract IEnumerable<object> GetAtomicValues();
 
-        protected ValueObject(IValidator<T> validator, T value)
+        public bool Equals(ValueObject other) => Equals((object)other);
+
+        public override bool Equals(object obj)
         {
-            if (validator == null)
-                throw new ValidatorNullException(GetType().FullName ?? String.Empty, ExceptionMessages.VALIDATOR_MUST_BE_SPECIFIED);
-            validator.Validate(value);
-            Value = value;
+            if (obj == null || obj.GetType() != GetType())
+                return false;
+
+            return GetAtomicValues()
+                .SequenceEqual(((ValueObject)obj).GetAtomicValues());
         }
 
-        public override string ToString()
-            => Value!.ToString() ?? GetType().ToString();
+        public override int GetHashCode() => 
+            GetAtomicValues()
+                .Aggregate(17, (current, obj) => 
+                    current * 23 + (obj?.GetHashCode() ?? 0));
 
-        public override int GetHashCode()
-            => Value!.GetHashCode();
-
-        public override bool Equals(object? other)
-            => Equals(other as ValueObject<T>);
-
-        public bool Equals(ValueObject<T>? other)
-        {
-            if (other is null)
-                return false;
-            if (ReferenceEquals(this, other))
-                return true;
-            if (GetType() != other.GetType())
-                return false;
-            return other.Value!.Equals(Value);
-        }
-
-        public static bool operator ==(ValueObject<T>? left, ValueObject<T>? right)
-            => Equals(left, right);
-
-        public static bool operator !=(ValueObject<T>? left, ValueObject<T>? right)
-            => !(left == right);
+        public static bool operator ==(ValueObject left, ValueObject right) => Equals(left, right);
+        public static bool operator !=(ValueObject left, ValueObject right) => !Equals(left, right);
     }
 }
