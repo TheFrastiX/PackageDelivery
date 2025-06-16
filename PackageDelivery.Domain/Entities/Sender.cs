f96dabc48b3b0b3ba2bfd0cf2efb168827d56a08
DimOn <dima05079@bk.ru>
2025-06-16 15:23:16 +0700

    Update Sender.cs

--- PackageDelivery.Domain/Entities/Sender.cs
+++ PackageDelivery.Domain/Entities/Sender.cs
@@ -1,18 +1,22 @@
-using System;
-using DeliveryParcel.Domain.Entities.Base;
 using DeliveryParcel.Domain.ValueObjects;
+using DeliveryParcel.Domain.Entities.Base;
 
 namespace DeliveryParcel.Domain.Entities
 {
+    /// <summary>
+    /// Представляет пользователя системы, который может отправлять посылки.
+    /// </summary>
     public class Sender : Entity<Guid>
     {
         public string FirstName { get; private set; }
         public string LastName { get; private set; }
-        public PhoneNumber PhoneNumber { get; private set; } // Value Object
-        public Address Address { get; private set; } // Value Object
+        public PhoneNumber PhoneNumber { get; private set; }
+        public Address Address { get; private set; }
         public string Email { get; private set; }
 
-        // Конструктор
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Sender"/> class.
+        /// </summary>
         public Sender(
             Guid id,
             string firstName,
@@ -29,31 +33,21 @@ namespace DeliveryParcel.Domain.Entities
             Email = email ?? throw new ArgumentNullException(nameof(email));
         }
 
-        // Для EF Core
+        /// <summary>
+        /// Защищённый конструктор для EF Core.
+        /// </summary>
         protected Sender() : base()
         {
         }
 
-        // Обновление контактной информации
-        public void UpdateContactInfo(
-            string firstName,
-            string lastName,
-            PhoneNumber phoneNumber,
-            Address address,
-            string email)
+        /// <summary>
+        /// Обновляет контактную информацию отправителя.
+        /// </summary>
+        public void UpdateContactInfo(PhoneNumber phoneNumber, Address address, string email)
         {
-            FirstName = firstName ?? throw new ArgumentNullException(nameof(firstName));
-            LastName = lastName ?? throw new ArgumentNullException(nameof(lastName));
             PhoneNumber = phoneNumber ?? throw new ArgumentNullException(nameof(phoneNumber));
             Address = address ?? throw new ArgumentNullException(nameof(address));
             Email = email ?? throw new ArgumentNullException(nameof(email));
-        }
-
-        // Изменение только номера телефона и адреса
-        public void UpdateBasicContactInfo(PhoneNumber phoneNumber, Address address)
-        {
-            PhoneNumber = phoneNumber ?? throw new ArgumentNullException(nameof(phoneNumber));
-            Address = address ?? throw new ArgumentNullException(nameof(address));
         }
     }
 }
