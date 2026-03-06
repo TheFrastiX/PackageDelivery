3310f681ab8de1cc2c046089ab74b4b7920d17c7
DimOn <dima05079@bk.ru>
2026-03-06 12:36:30 +0700

    Update Entity.cs

--- HotelBooking/Domain/HotelBooking.Domain/Base/Entity.cs
+++ HotelBooking/Domain/HotelBooking.Domain/Base/Entity.cs
@@ -1,8 +1,24 @@
 namespace HotelBooking.Domain.Base;
 
+/// <summary>
+/// Represents an entity in the system.
+/// </summary>
+/// <typeparam name="TId">The type of the entity's ID.</typeparam>
+/// <param name="id">The ID of the entity.</param>
+/// <remarks>
+/// Initializes a new instance of the <see cref="Entity{TId}"/> class.
+/// </remarks>
 public abstract class Entity<TId>(TId id) where TId : struct, IEquatable<TId>
 {
+    /// <summary>
+    /// Gets the ID of the entity.
+    /// </summary>
     public TId Id { get; } = id;
-    
-    protected Entity() : this(default!) { }
+
+    /// <summary>
+    /// Protected constructor for Entity Framework or other ORM if needed.
+    /// </summary>
+    protected Entity() : this(default!)
+    {
+    }
 }
