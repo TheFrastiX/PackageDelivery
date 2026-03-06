f6ac9f65800646cb10d565bac766bdfe2d435d86
DimOn <dima05079@bk.ru>
2026-03-06 11:30:08 +0700

    Update GuestName.cs

--- HotelBooking/Domain/HotelBooking.ValueObjects/GuestName.cs
+++ HotelBooking/Domain/HotelBooking.ValueObjects/GuestName.cs
@@ -1,13 +1,6 @@
-public class GuestName(string name) : ValueObject<string>(new GuestNameValidator(), name);
+using HotelBooking.ValueObjects.Base;
+using HotelBooking.ValueObjects.Validators;
 
-// Validator:
-public class GuestNameValidator : IValidator<string>
-{
-    public void Validate(string value)
-    {
-        if (string.IsNullOrWhiteSpace(value))
-            throw new InvalidValueException(nameof(GuestName), "Name cannot be empty");
-        if (value.Length < 2 || value.Length > 100)
-            throw new InvalidValueException(nameof(GuestName), "Name must be 2-100 characters");
-    }
-}
+namespace HotelBooking.ValueObjects;
+
+public class GuestName(string name) : ValueObject<string>(new GuestNameValidator(), name);
