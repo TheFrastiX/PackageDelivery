fec2f2f9e8d28917694a08b3f2228c314035da08
DimOn <dima05079@bk.ru>
2026-03-06 19:57:43 +0700

    Update EmailValidator.cs

--- HotelBooking/Domain/HotelBooking.ValueObjects/Validators/EmailValidator.cs
+++ HotelBooking/Domain/HotelBooking.ValueObjects/Validators/EmailValidator.cs
@@ -3,21 +3,7 @@ using System.Text.RegularExpressions;
 
 namespace HotelBooking.ValueObjects.Validators;
 
-public class EmailValidator : IValidator<string>
-{
-    public void Validate(string value)
-    {
-        if (string.IsNullOrWhiteSpace(value))
-            throw new ArgumentException("Email cannot be empty", nameof(value));
-        
-        var emailPattern = @"^[^@\s]+@[^@\s]+\.[^@\s]+$";
-        if (!Regex.IsMatch(value, emailPattern))
-            throw new ArgumentException("Invalid email format", nameof(value));
-        
-        if (value.Length > 255)
-            throw new ArgumentException("Email must not exceed 255 characters", nameof(value));
-    }
-}
+
 public class EmailValidator : IValidator<string>
 {
     public const int MAX_LENGTH = 255;
