99cbc3bece8acb2e57e5199e95b8ae6637a8af33
DimOn <dima05079@bk.ru>
2026-03-06 19:15:01 +0700

    Update RoomNumberValidator.cs

--- HotelBooking/Domain/HotelBooking.ValueObjects/Validators/RoomNumberValidator.cs
+++ HotelBooking/Domain/HotelBooking.ValueObjects/Validators/RoomNumberValidator.cs
@@ -13,3 +13,16 @@ public class RoomNumberValidator : IValidator<string>
             throw new ArgumentException("Room number must be between 1 and 10 characters", nameof(value));
     }
 }
+public class RoomNumberValidator : IValidator<string>
+{
+    public const int MAX_LENGTH = 10;
+    
+    public void Validate(string value)
+    {
+        if (string.IsNullOrWhiteSpace(value))
+            throw new ArgumentException("Room number cannot be empty", nameof(value));
+        
+        if (value.Length < 1 || value.Length > MAX_LENGTH)
+            throw new ArgumentException($"Room number must be between 1 and {MAX_LENGTH} characters", nameof(value));
+    }
+}
